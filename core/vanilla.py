f649e91f65e0730d21d9be351f5c6c5813b99c5b
Maéva Cavaleri <cavalerimaeva@gmail.com>
2026-01-09 19:09:37 +0100

    Create vanilla.py

---
+++ core/vanilla.py
@@ -0,0 +1,38 @@
+import numpy as np 
+from .base import Option
+
+class CallOption(Option) :
+  """
+  Cette classe représente une option d'achat (Call) européenne.
+  Une option Call donne au détenteur le droit, mais pas l'obligation d'acheter 
+  un actif sous-jacent à un prix d'exercice fixé (K) à une date (T).
+  L'investisseur est 'haussier' (bullish) : il profite si le prix du marché 
+  à l'échéance (s_T) est supérieur au strike (K).
+  """
+
+  def __init__(self,strike,expiry,premium):
+    super().__init__(strike,expiry,premium)
+
+  def payoff(self, s_T) :
+    """
+    Calcule le gain brut à l'échéance pour un Call.
+    """
+    return np.maximum(0,s_T-self.K)
+
+class PutOption(Option) :
+  """
+  Cette classe représente une option de vente (Put) européenne.
+  Une option Put donne au détenteur le droit, mais pas l'obligation de vendre 
+  un actif sous-jacent à un prix d'exercice fixé (K) à une date (T).
+  L'investisseur est 'baissier' (bearish) : il profite si le prix du marché 
+  à l'échéanc (s_T) tombe en dessous du strike (K).
+  """
+  
+  def __init__(self,strike,expiry,premium):
+    """
+    Calcule le gain brut à l'échéance pour un Put.
+    """
+    super().__init__(strike,expiry,premium)
+
+  def payoff(self,s_T) :
+     return np.maximum(0,self.K-s_T)
